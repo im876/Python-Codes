b45327391f20f2e4e60a6ce098818fd65b459803
Ishan Modi <64376463+im876@users.noreply.github.com>
2021-07-08 13:06:02 +0530

    Update 002. Find the Runner-Up Score.py

--- HackerRank-Python Practice/02. Basic Data Types/002. Find the Runner-Up Score.py
+++ HackerRank-Python Practice/02. Basic Data Types/002. Find the Runner-Up Score.py
@@ -1,3 +1,4 @@
-n = int(input())
-arr = list(map(int, input().split()))
-print(max([x for x in arr if x != max(arr)]))
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    print(sorted(set(arr), reverse=True)[1])
