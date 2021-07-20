0b7f2617b54dc5eee6a255f9d2d0cc88d6d0d921
Ishan Modi <64376463+im876@users.noreply.github.com>
2021-07-20 21:01:09 +0530

    Create sweet seventeen.py

---
+++ Codes/sweet seventeen.py
@@ -0,0 +1,12 @@
+n = input().strip()
+i = int(n,17)
+print(i)
+
+
+#Given a maximum of four digit to the base 17(10 -> A, 11 -> B, 12 -> C, 16 -> G) as input, output its decimal value.
+#Input : 23GF
+#Output : 10980
+
+
+#Input : 1A
+#Output : 27
