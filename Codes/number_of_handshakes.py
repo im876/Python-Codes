5655a7dceb15e780bfaf37746bc68072892e46b2
Ishan Modi <64376463+im876@users.noreply.github.com>
2021-08-21 09:07:14 +0530

    Create number_of_handshakes.py

---
+++ Codes/number_of_handshakes.py
@@ -0,0 +1,6 @@
+#take user inputs
+N = int(input('Enter number of people available :'))
+#formula
+no_of_handshakes = int(N *((N-1)/2))
+#print number of no_of_handshakes
+print('Maximum number of handshakes can be :' + str(no_of_handshakes))
