fd90c24c3d19e1ae2f392b34aeef24ab7721593b
Ishan Modi <64376463+im876@users.noreply.github.com>
2021-08-21 09:12:54 +0530

    Create area_circle.py

---
+++ Codes/area_circle.py
@@ -0,0 +1,10 @@
+from math import pi
+r=float(input("Enter radius of circle :"))
+area=pi*r*r
+print("The area of circle is",end=" ")
+print(area)
+from math import pi
+d=float(input("Enter diameter of circle :"))
+area=(pi*d*d)/4
+print("The area of circle is",end="")
+print(area)
