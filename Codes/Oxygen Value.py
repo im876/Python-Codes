3e42ac9df56eb79ba31b722b1dc664db8a14442d
Ishan Modi <64376463+im876@users.noreply.github.com>
2021-08-28 10:55:41 +0530

    Update Oxygen Value.py

--- Codes/Oxygen Value.py
+++ Codes/Oxygen Value.py
@@ -23,7 +23,7 @@ if(A2>=A1 and A2>=A3):
 if(A3>=A2 and A3>=A1):
     print("Trainee Number: 3")
     
-    
+'''   
 #The selection of MPCS exams includes a fitness test which is conducted on the ground. There will be a batch of 3 trainees, appearing for a running test on track for 3 rounds.
 
 #You need to record their oxygen level after every round. After trainees are finished with all rounds, calculate for each trainee his average oxygen level over the 3 rounds and select the one with the highest average oxygen level as the fittest trainee. If more than one trainee attains the same highest average level, they all need to be selected. Display the fittest trainee(or trainers) and the highest average oxygen level.
@@ -71,3 +71,4 @@ if(A3>=A2 and A3>=A1):
 
 
 Oxygen must be in the given format as in the above example. For any wrong input, the final output should display “INVALID INPUT”
+'''
