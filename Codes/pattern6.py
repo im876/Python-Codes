2cb42a7c46cfc3888c6bf5d70cf4db19fe5ea110
Ishan Modi <64376463+im876@users.noreply.github.com>
2021-09-19 21:34:34 +0530

    Create pattern6.py

---
+++ Codes/pattern6.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+
+for i in range(1, a+1):
+    for j in range(1,b+1):
+        print("*",end = "")
+    print()
+    
+'''
+4
+5
+*****
+*****
+*****
+*****
+'''
